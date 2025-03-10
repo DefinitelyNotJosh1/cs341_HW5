87aa1f959851e78d1ce49022ebdbbc122aeade26
DefinitelyNotJosh1 <157744625+DefinitelyNotJosh1@users.noreply.github.com>
2025-03-10 14:51:25 -0700

    automatically display database info

--- hw5/public/javascripts/dropdown.js
+++ hw5/public/javascripts/dropdown.js
@@ -6,17 +6,17 @@ $(document).ready(function() {
         $('#dropdownButton').text(defaultMonth); // set default month
 
         // automatically load orders for default month
-        // $.post('/orders', { month: defaultMonth }, function(data) {
+        $.post('/orders', { month: defaultMonth }, function(data) {
 
-        //     // get order list and clear it
-        //     var ordersList = $('#ordersList');
-        //     ordersList.empty();
+            // get order list and clear it
+            var ordersList = $('#ordersList');
+            ordersList.empty();
 
-        //     data.forEach(function(order) {
-        //         var orderItem = $('<li></li>').text(order.quantity + ' ' + order.topping);
-        //         ordersList.append(orderItem);
-        //     });
-        // });
+            data.forEach(function(order) {
+                var orderItem = $('<li></li>').text(order.quantity + ' ' + order.topping);
+                ordersList.append(orderItem);
+            });
+        });
 
     // enable scrolling with mouse wheel when hovering over the dropdown
     $('.dropdown-content').on('mousewheel DOMMouseScroll', function(e) {
